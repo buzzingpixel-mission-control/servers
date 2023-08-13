051907c55740b6d069bd0f33b2b13af33832a975
TJ Draper <tj@buzzingpixel.com>
2023-08-13 11:46:30 -0500

    Add endpoints for ssh keys list

--- src/EventListeners/Routing.php
+++ src/EventListeners/Routing.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace MissionControlServers\EventListeners;
 
 use MissionControlBackend\Http\ApiApplyRoutesEvent;
+use MissionControlServers\SshKeys\GetSshKeysListAction;
+use MissionControlServers\SshKeys\GetSshKeysListArchivedAction;
 
 class Routing
 {
     public function onApplyRoutes(ApiApplyRoutesEvent $event): void
     {
+        GetSshKeysListAction::registerRoute($event);
+        GetSshKeysListArchivedAction::registerRoute($event);
     }
 }
---
+++ src/SshKeys/GetSshKeysListArchivedAction.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MissionControlServers\SshKeys;
+
+use MissionControlBackend\Http\ApplyRoutesEvent;
+use MissionControlBackend\Persistence\Sort;
+use MissionControlIdp\Authorize\RequireAdminMiddleware;
+use MissionControlIdp\Authorize\ResourceServerMiddlewareWrapper;
+use MissionControlServers\SshKeys\Persistence\FindSshKeyParameters;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+use function json_encode;
+
+use const JSON_PRETTY_PRINT;
+
+readonly class GetSshKeysListArchivedAction
+{
+    public static function registerRoute(ApplyRoutesEvent $event): void
+    {
+        $event->get('/ssh-keys/list/archived', self::class)
+            ->add(RequireAdminMiddleware::class)
+            ->add(ResourceServerMiddlewareWrapper::class);
+    }
+
+    public function __construct(private SshKeyRepository $repository)
+    {
+    }
+
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+    ): ResponseInterface {
+        $items = $this->repository->findAll(
+            FindSshKeyParameters::create()
+                ->withIsActive(false)
+                ->withOrderBy('title')
+                ->withSort(Sort::ASC),
+        );
+
+        $response = $response->withHeader(
+            'Content-type',
+            'application/json',
+        );
+
+        $response->getBody()->write((string) json_encode(
+            $items->asArray(),
+            JSON_PRETTY_PRINT,
+        ));
+
+        return $response;
+    }
+}
